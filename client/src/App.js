669aa88caddde5f665fae511a0cafd04b1f46f70
Christian <christian_hane@aol.com>
2018-06-25 19:55:35 -0700

    small changes

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
             <div>
               <Switch>
                 <Route exact path="/" component={Home}/>
+                <Route exact path="/admin" component={Admin}/>
               </Switch>
             </div>
 

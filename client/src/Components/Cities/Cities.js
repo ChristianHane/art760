d6b53a10e8d0b4df569993ac6cd002acc4738043
MirandaAH <hanemiranda@gmail.com>
2018-07-27 19:17:02 -0700

    cities update

--- client/src/Components/Cities/Cities.js
+++ client/src/Components/Cities/Cities.js
@@ -4,81 +4,81 @@ import MediaQuery from 'react-responsive';
 class Cities extends Component {
   render(){
     return(
-      <div>
+      <div className="cities">
         <p className="intro">THIS IS A COLOR CODED ALPHABETICAL LISTING OF ALL THE VENUES, GALLERIES, SHOPS, RESTAURANTS AND EVERYTHING ELSE THE PALM SPRINGS AREA HAS TO OFFER</p>
         <MediaQuery query='(min-width: 768px)'>
           <div className="row">
             <div className="col-4">
-              <button className="btn here PalmSprings">PALM SPRINGS</button>
+              <button className="btn here2 PalmSprings">PALM SPRINGS</button>
               <br></br>
-              <button className="btn here RanchoMirage">RANCHO MIRAGE</button>
+              <button className="btn here2 RanchoMirage">RANCHO MIRAGE</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here PalmDesert">PALM DESERT</button>
+              <button className="btn here2 PalmDesert">PALM DESERT</button>
               <br></br>
-              <button className="btn here CathedralCity">CATHEDRAL CITY</button>
+              <button className="btn here2 CathedralCity">CATHEDRAL CITY</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here IndianWells">INDIAN WELLS</button>
+              <button className="btn here2 IndianWells">INDIAN WELLS</button>
               <br></br>
-              <button className="btn here LaQuinta">LA QUINTA</button>
+              <button className="btn here2 LaQuinta">LA QUINTA</button>
             </div>
           </div>
           <div className="row">
             <div className="col-4">
-              <button className="btn here Indio">INDIO</button>
+              <button className="btn here2 Indio">INDIO</button>
               <br></br>
-              <button className="btn here Idyllwild">IDYLLWILD</button>
+              <button className="btn here2 Idyllwild">IDYLLWILD</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here HighDesert">HIGH DESERT</button>
+              <button className="btn here2 HighDesert">HIGH DESERT</button>
               <br></br>
-              <button className="btn here DesertHotSprings">DESERT HOT SPRINGS</button>
+              <button className="btn here2 DesertHotSprings">DESERT HOT SPRINGS</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here CoachellaValley">COACHELLA VALLEY</button>
+              <button className="btn here2 CoachellaValley">COACHELLA VALLEY</button>
             </div>
           </div>
         </MediaQuery>
         <MediaQuery query='(max-width: 767px)'>
           <div className="row">
             <div className="col-4">
-              <button className="btn here PalmSprings">PALM SPRINGS</button>
+              <button className="btn here2 PalmSprings">PALM SPRINGS</button>
               <br></br>
-              <button className="btn here RanchoMirage">RANCHO MIRAGE</button>
+              <button className="btn here2 RanchoMirage">RANCHO MIRAGE</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here PalmDesert">PALM DESERT</button>
+              <button className="btn here2 PalmDesert">PALM DESERT</button>
               <br></br>
-              <button className="btn here CathedralCity">CATHEDRAL CITY</button>
+              <button className="btn here2 CathedralCity">CATHEDRAL CITY</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here IndianWells">INDIAN WELLS</button>
+              <button className="btn here2 IndianWells">INDIAN WELLS</button>
               <br></br>
-              <button className="btn here LaQuinta">LA QUINTA</button>
+              <button className="btn here2 LaQuinta">LA QUINTA</button>
             </div>
           </div>
           <div className="row">
             <div className="col-4">
-              <button className="btn here Indio">INDIO</button>
+              <button className="btn here2 Indio">INDIO</button>
               <br></br>
-              <button className="btn here Idyllwild">IDYLLWILD</button>
+              <button className="btn here2 Idyllwild">IDYLLWILD</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here HighDesert">HIGH DESERT</button>
+              <button className="btn here2 HighDesert">HIGH DESERT</button>
               <br></br>
-              <button className="btn here DesertHotSprings">DESERT HOT SPRINGS</button>
+              <button className="btn here2 DesertHotSprings">DESERT HOT SPRINGS</button>
             </div>
 
             <div className="col-4">
-              <button className="btn here CoachellaValley">COACHELLA VALLEY</button>
+              <button className="btn here2 CoachellaValley">COACHELLA VALLEY</button>
             </div>
           </div>
         </MediaQuery>

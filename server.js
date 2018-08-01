60c402975aa39e09c5d8294a86b16afbe47323ec
Christian <christian_hane@aol.com>
2018-07-31 17:11:10 -0700

    update

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const businessRouter = require('./routes/BusinessRouter');
+const businessRouter = require('./routes/businessRouter');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
